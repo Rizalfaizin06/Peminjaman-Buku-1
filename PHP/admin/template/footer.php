d67b946dd5b40a35b6beaa316303d1ff0774a886
Rizalfaizin06 <rizalfaizin06@gmail.com>
2022-07-04 12:55:14 +0700

    commit linux

--- PHP/admin/template/footer.php
+++ PHP/admin/template/footer.php
@@ -36,10 +36,8 @@
         <!-- Copyright -->
     </footer>
 
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script script src="assets/js/jquery-3.6.0.min.js"></script>
-    <script script src="assets/js/script.js"></script>
-
+    <script src="assets/js/bootstrap.bundle.min.js">
+    </script>
     </body>
 
     </html>
